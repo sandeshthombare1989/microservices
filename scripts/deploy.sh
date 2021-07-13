f04ee7e62da29a20810e58a058bf6d5328810442
sandeshthombare1989 <sandeshthombare1989@users.noreply.github.com>
2021-07-13 19:40:12 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,13 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 /usr/local/bin/kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
-  --insecure-skip-tls-verify=true \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
   apply -f ./kube/
 echo "The build number is ${TRAVIS_BUILD_NUMBER}"
 /usr/local/bin/kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
-  ---insecure-skip-tls-verify=true \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
   set image deployment/microservice-python microservice-python=coolsandythombare/microservice-python:${TRAVIS_BUILD_NUMBER} --record
